aac7962d72f4cbc5b51f51a9f4e8833fc5c8cf64
JanKl <i13015@hb.dhbw-stuttgart.de>
2016-05-08 14:13:20 +0200

    Implemented ticket overview on client side

---
+++ public/javascripts/ticketCommon.js
@@ -0,0 +1,15 @@
+// http://stackoverflow.com/a/12034334
+ var entityMap = {
+  "&": "&amp;",
+  "<": "&lt;",
+  ">": "&gt;",
+  '"': '&quot;',
+  "'": '&#39;',
+  "/": '&#x2F;'
+};
+
+function escapeHtml(string) {
+  return String(string).replace(/[&<>"'\/]/g, function (s) {
+    return entityMap[s];
+  });
+}
---
+++ public/javascripts/ticketOverview.js
@@ -0,0 +1,23 @@
+function updateOverviewBoard() {
+  $.getJSON('/waitingTicket', function done(allWaitingTickets) {
+    $('#waitingTicketsTable tbody').empty();
+    
+    var allWaitingTicketsCount = allWaitingTickets.length;
+    for (var i = 0; i < allWaitingTicketsCount; ++i) {
+      var deskNumberOutput = '(waiting)';
+      var classOutput = '';
+      
+      if (allWaitingTickets[i]['deskNumber'] != 0) {
+        deskNumberOutput = allWaitingTickets[i]['deskNumber'];
+        var classOutput = 'success';
+      }
+      
+      $('#waitingTicketsTable tbody:last-child').append('<tr class="' + classOutput + '"><td>' + escapeHtml(allWaitingTickets[i]['callNumber']) + '</td><td>' + escapeHtml(deskNumberOutput) + '</td></tr>');
+    }      
+  });
+}
+
+$(function ready() {
+  updateOverviewBoard();
+  setInterval(updateOverviewBoard, 1000);
+});
